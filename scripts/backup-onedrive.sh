acefa2a2dd8bfe8ee9c6d8a770f9e5c24383b733
Shafqat Ahmed <shafqatahmed@gmail.com>
2016-09-17 04:50:26 +0600

    backup script added

---
+++ scripts/backup-onedrive.sh
@@ -0,0 +1,3 @@
+backup_name=$(date +%F-%H-%M-%S).zip
+zip -r /root/backup/$backup_name /root/persist
+rclone copy /root/backup/$backup_name onedrive:/cloud-backup/$backup_name
--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
 # install some basic stuff
 
-apt-get install mc
-chmod +x ./scripts/docker-compose-install.sh
-chmod +x ./scripts/node-and-tools-install.sh
-chmod +x ./scripts/docker-images-pull.sh
+apt-get install mc zip
 ./scripts/docker-compose-install.sh
 ./scripts/node-and-tools-install.sh
 ./scripts/docker-images-pull.sh
+sh ./scripts/docker-compose-install.sh
+sh ./scripts/node-and-tools-install.sh
+sh ./scripts/docker-images-pull.sh
+cd /root
+wget http://downloads.rclone.org/rclone-current-linux-amd64.zip
+unzip rclone-current-linux-amd64.zip
+cd rclone-current-linux-amd64
+cp rclone /usr/bin/rclone
+
+#date +%F-%H-%M-%S
