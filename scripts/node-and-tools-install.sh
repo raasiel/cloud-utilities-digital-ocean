5f4455ad9cb3ab14347773040c72e1c086c0d41b
Shafqat Ahmed <shafqatahmed@gmail.com>
2016-09-17 02:33:53 +0600

    Added node and setup files

---
+++ scripts/node-and-tools-install.sh
@@ -0,0 +1,3 @@
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+apt-get install -y nodejs
+sudo apt-get install -y build-essential
---
+++ setuo.sh
@@ -0,0 +1,5 @@
+# install some basic stuff
+
+apt-get install mc
+./scripts/docker-compose-install.sh
+./scripts/node-and-tools-install.sh
