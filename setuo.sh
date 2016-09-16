726c85856c88161f315e6fdd3698285705b32a43
Shafqat Ahmed <shafqatahmed@gmail.com>
2016-09-17 02:37:20 +0600

    commit

--- setuo.sh
+++ setuo.sh
@@ -1,5 +1,7 @@
 # install some basic stuff
 
 apt-get install mc
+chmod +x ./scripts/docker-compose-install.sh
+chmod +x ./scripts/node-and-tools-install.sh
 ./scripts/docker-compose-install.sh
 ./scripts/node-and-tools-install.sh
