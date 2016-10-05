6afad41718ecee65aeefd237c5acbeae8720bea0
Shafqat Ahmed <shafqatahmed@gmail.com>
2016-10-05 17:55:10 +0600

    update

---
+++ scripts/install-java-tfs-cli.sh
@@ -0,0 +1,7 @@
+# create and move to folder
+wget https://download.microsoft.com/download/8/F/6/8F68DDC8-4E75-4BEA-951E-C14BFF336E81/TEE-CLC-14.0.3.zip
+#unzip TEE-CLC-14.0.3.zip -d ./cli
+sudo apt-get install python-software-properties
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
