28cbc98fcf1635d6c175ab389643a24234d60bb0
Shafqat Ahmed <Shafqat Ahmed>
2016-10-12 16:25:47 +0600

    udoate scriots

---
+++ scripts/docker-build-all.sh
@@ -0,0 +1,4 @@
+docker build ../docker/net-java -t raasiel/net-java
+docker build ../docker/net-java-node -t raasiel/net-java-node
+docker build ../docker/dev -t raasiel/dev
+docker build ../docker/stockbase -t raasiel/stockbase
